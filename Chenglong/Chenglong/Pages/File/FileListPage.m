d6431980430d7642e93514b93031e7c002c6a3a8
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-06-12 23:17:33 -0700

    delete course

--- Chenglong/Chenglong/Pages/File/FileListPage.m
+++ Chenglong/Chenglong/Pages/File/FileListPage.m
@@ -27,6 +27,33 @@
     return 1;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        CourseDetails* item = [self.courseDetailsList.items objectAtIndex:indexPath.row];
+        [CourseApi CourseAPI_RemoveCourse:item.course.id onSuccess:^(Course *resp) {
+            [self.courseDetailsList.items removeObjectAtIndex:indexPath.row];
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            [self save];
+        } onError:^(APIError *err) {
+            
+        }];
+    }
+}
+
+-(void)save {
+    NSString* json = [self.courseDetailsList toJson];
+    [json writeToFile:[self jsonFileName ] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+-(NSString*)jsonFileName {
+    NSString* fileName = [self.filePath stringByAppendingFormat:@"/%@.json", self.courseDetailsList.courseDetails.course.id];
+    return fileName;
+}
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.courseDetailsList.items.count;
 }
