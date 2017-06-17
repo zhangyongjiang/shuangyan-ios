c4098ad6fe23c644e65ecab6295c3f82393bb469
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-06-16 22:33:37 -0700

    FileManager getDirForId

---
+++ Chenglong/Chenglong/Model/FileManager.h
@@ -0,0 +1,15 @@
+//
+//  FileManager.h
+//  Chenglong
+//
+//  Created by Kevin Zhang (BCG DV) on 6/16/17.
+//  Copyright © 2017 Chenglong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileManager : NSObject
+
++(void)saveCourseDetails:(CourseDetails*)courseDetails;
+
+@end
