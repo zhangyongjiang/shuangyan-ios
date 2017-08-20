3798e1f558d5b46984cd8e0aa040293a610e95a4
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-08-19 19:54:25 -0700

    LocalMediaContentShardGroup

---
+++ Chenglong/Chenglong/Model/LocalMediaContentShardGroup.h
@@ -0,0 +1,17 @@
+//
+//  LocalMediaContentShardGroup.h
+//  Chenglong
+//
+//  Created by Kevin Zhang (BCG DV) on 8/19/17.
+//  Copyright © 2017 Chenglong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LocalMediaContentShard.h"
+
+@interface LocalMediaContentShardGroup : NSObject
+
+-(id)initWithShards:(NSArray<LocalMediaContentShard*>*)shards;
+-(void)download;
+
+@end
---
+++ Chenglong/Chenglong/Model/LocalMediaContentShardGroup.m
@@ -0,0 +1,13 @@
+//
+//  LocalMediaContentShardGroup.m
+//  Chenglong
+//
+//  Created by Kevin Zhang (BCG DV) on 8/19/17.
+//  Copyright © 2017 Chenglong. All rights reserved.
+//
+
+#import "LocalMediaContentShardGroup.h"
+
+@implementation LocalMediaContentShardGroup
+
+@end
