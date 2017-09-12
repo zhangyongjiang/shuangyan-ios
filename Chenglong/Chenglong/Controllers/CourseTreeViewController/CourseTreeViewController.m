2f8e578ff7da96f5316c4874282e764b3077dc20
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-09-12 07:00:35 -0700

    handle menu events

--- Chenglong/Chenglong/Controllers/CourseTreeViewController/CourseTreeViewController.m
+++ Chenglong/Chenglong/Controllers/CourseTreeViewController/CourseTreeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "CourseTreeViewController.h"
+#import "CreateFileViewController.h"
+#import "BaseNavigationController.h"
 
 @interface CourseTreeViewController ()
 
@@ -40,26 +42,101 @@
 -(NSMutableArray*)getTopRightMenuItems {
     NSMutableArray* menuItems = [[NSMutableArray alloc] init];
     CourseDetails* selected = [self.page.treeView itemForSelectedRow];
-    if(!selected.course.isDir.boolValue) {
-        [menuItems addObject:[[MenuItem alloc] initWithText:@"播放" andImgName:@"file_item_play_icon"]];
-        [menuItems addObject:[[MenuItem alloc] initWithText:@"删除" andImgName:@"file_item_remove_icon"]];
-        [menuItems addObject:[[MenuItem alloc] initWithText:@"改名" andImgName:@"file_item_edit_icon"]];
-        [menuItems addObject:[[MenuItem alloc] initWithText:@"移动" andImgName:@"file_item_exchange_icon"]];
-    }
-    else {
+    if(selected.course.isDir.boolValue) {
         [menuItems addObject:[[MenuItem alloc] initWithText:@"新文件" andImgName:@"file_item_newFile_icon"]] ;
         [menuItems addObject:[[MenuItem alloc] initWithText:@"新文件夹" andImgName:@"file_item_newfolder_icon"] ];
         [menuItems addObject:[[MenuItem alloc] initWithText:@"删除" andImgName:@"file_item_remove_icon"]];
         [menuItems addObject:[[MenuItem alloc] initWithText:@"改名" andImgName:@"file_item_edit_icon"]];
         [menuItems addObject:[[MenuItem alloc] initWithText:@"移动" andImgName:@"file_item_exchange_icon"]];
+        if(selected.items.count) {
+            [menuItems addObject:[[MenuItem alloc] initWithText:@"播放" andImgName:@"file_item_play_icon"]];
+        }
+    }
+    else {
         [menuItems addObject:[[MenuItem alloc] initWithText:@"播放" andImgName:@"file_item_play_icon"]];
+        [menuItems addObject:[[MenuItem alloc] initWithText:@"删除" andImgName:@"file_item_remove_icon"]];
+        [menuItems addObject:[[MenuItem alloc] initWithText:@"改名" andImgName:@"file_item_edit_icon"]];
+        [menuItems addObject:[[MenuItem alloc] initWithText:@"移动" andImgName:@"file_item_exchange_icon"]];
     }
     return menuItems;
 }
 
 -(void)topRightMenuItemClicked:(NSString *)cmd {
     NSLog(@"cmd %@ clicked", cmd);
+    CourseDetails* selected = [self.page.treeView itemForSelectedRow];
+    if(selected.course.isDir.boolValue) {
+        if ([cmd isEqualToString:@"新文件"]) {
+            CreateFileViewController *file = [CreateFileViewController loadFromNib];
+            file.parentCourseId = selected.course.id;
+            BaseNavigationController *nav = [[BaseNavigationController alloc] initWithRootViewController:file];
+            [self.navigationController presentViewController:nav animated:YES completion:nil];
+        }
+        else if([cmd isEqualToString:@"新文件夹"]){
+            //新建文件夹
+            [self creatCourseDir:selected];
+        }
+        else if ([cmd isEqualToString:@"改名"]){
+            //改名
+            [self changeCourseName:selected];
+        }
+        else if ([cmd isEqualToString:@"移动"]){
+            [self moveCourse:selected];
+        }
+        else if ([cmd isEqualToString:@"播放"]){
+            [self playCourse:selected];
+        }
+        else if ([cmd isEqualToString:@"删除"]) {
+            if(selected.items.count>0) {
+                [self presentFailureTips:@"当前文件夹为空时才能删除"];
+                return;
+            }
+            [self removeCourse:selected];
+        }
+    } else {
+        if([cmd isEqualToString:@"删除"]) {
+            [self removeCourse:selected];
+        }
+        else if([cmd isEqualToString:@"改名"]) {
+        }
+        else if([cmd isEqualToString:@"移动"]) {
+        }
+        else if([cmd isEqualToString:@"播放"]) {
+        }
+    }
 }
+
+-(void)creatCourseDir:(CourseDetails*)cd {
+}
+
+-(void)changeCourseName:(CourseDetails*)cd {
+}
+
+-(void)playCourse:(CourseDetails*)cd {
+}
+
+-(void)moveCourse:(CourseDetails*)cd {
+}
+
+-(void)removeCourse:(CourseDetails*)cd {
+    WeakSelf(weakSelf)
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"删除" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *actionCancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [alert addAction:actionCancel];
+    UIAlertAction *actionSure = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [CourseApi CourseAPI_RemoveCourse:cd.course.id onSuccess:^(Course *resp) {
+            [weakSelf presentFailureTips:@"删除成功"];
+            [[NSNotificationCenter defaultCenter] postNotificationName:NotificationCourseChanged object:resp];
+            [weakSelf.navigationController popViewControllerAnimated:YES];
+        } onError:^(APIError *err) {
+            ALERT_VIEW_WITH_TITLE(err.errorCode, err.errorMsg);
+        }];
+    }];
+    [alert addAction:actionSure];
+    [[self getCurrentNavController] presentViewController:alert animated:YES completion:^{
+    }];
+}
+
 
 -(void)refreshPage {
     [MyHTTPSessionManager disableCacheForSeconds:3];
