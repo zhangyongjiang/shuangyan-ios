40cf3c82d0fe3bab75ae9683b42900c4c76ee3d7
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-11-06 05:38:31 -0800

    UserCourseTreePage

---
+++ Chenglong/Chenglong/Controllers/CourseTreeViewController/UserCourseTreePage.h
@@ -0,0 +1,13 @@
+//
+//  UserCourseTreePage.h
+//  Chenglong
+//
+//  Created by Kevin Zhang (BCG DV) on 11/6/17.
+//  Copyright © 2017 Chenglong. All rights reserved.
+//
+
+#import "CourseTreePage.h"
+
+@interface UserCourseTreePage : CourseTreePage
+
+@end
---
+++ Chenglong/Chenglong/Controllers/CourseTreeViewController/UserCourseTreePage.m
@@ -0,0 +1,21 @@
+//
+//  UserCourseTreePage.m
+//  Chenglong
+//
+//  Created by Kevin Zhang (BCG DV) on 11/6/17.
+//  Copyright © 2017 Chenglong. All rights reserved.
+//
+
+#import "UserCourseTreePage.h"
+
+@implementation UserCourseTreePage
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
